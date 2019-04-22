5ef15ab20dd9a2f829a7b1d74f491c1a1ca08bec
Tom Bell <tjbell@gmail.com>
2019-04-22 14:53:19 -0600

    test: init packages

--- init.R
+++ init.R
@@ -2,4 +2,4 @@ install.packages('packrat')
 
 packrat::init()
 
-install.packages(c('ggplot2', 'dplyr', 'tidyr', 'readr', 'testthat', 'xml2', 'optparse'))
+install.packages(c('ggplot2', 'dplyr', 'tidyr', 'readr', 'stringr', 'testthat', 'xml2', 'optparse', 'mockery', 'assertthat', 'checkmate', 'assertr', 'import', 'lobstr', 'pryr'))
