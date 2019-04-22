54f7cd5c4ec7444a97986fbf577a5cd334ec7005
Tom Bell <tjbell@gmail.com>
2019-04-22 16:30:05 -0600

    test: run_tests changes

--- run_tests.R
+++ run_tests.R
@@ -1,13 +1,13 @@
 tests <- function(module = 'all', reporter = 'tap') {
-  if ('testthat' %in% rownames(installed.packages())) {
+  if ('testthat' %in% rownames(utils::installed.packages())) {
     library('testthat')
-    library('mockery')
+    library('checkmate')
     if (module == 'module1') {
       fpath <- paste0('tests/tests_', module, '.R')
       if(test_file_exists(fpath)) {
         test_file(fpath, reporter = reporter)
       } else {
-        print('The test file is missing check your `tests` directory.') 
+        print('The test file is missing check your `tests` directory.')
       }
     } else if (module == 'all') {
       test_dir('tests', reporter = reporter)
@@ -20,14 +20,12 @@ tests <- function(module = 'all', reporter = 'tap') {
 }
 
 if (!interactive()) {
-  source('tests/reporter-json.R')
   library('optparse')
+  library('testthat')
   option_list <- list(
-    make_option(c('-m', '--module'), type = 'character', default = 'all',
-                help='Module to test.'),
     make_option(c('-r', '--reporter'), type = 'character', default = 'junit',
                 help='Reporter to use.')
   )
   opt <- parse_args(OptionParser(option_list=option_list))
-  test_results <- tests(opt$module, opt$reporter)
+  test_dir(paste0(getwd(), '/tests/'), reporter = opt$reporter)
 }
