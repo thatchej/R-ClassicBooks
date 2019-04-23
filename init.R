94fda011b770136ded53b72d892b1ced9d61ea79
Tom Bell <tjbell@gmail.com>
2019-04-23 10:39:35 -0600

    chore: delete unneeded files

--- init.R
+++
@@ -1,5 +0,0 @@
-install.packages('packrat')
-
-packrat::init()
-
-install.packages(c('ggplot2', 'dplyr', 'tidyr', 'readr', 'stringr', 'testthat', 'xml2', 'optparse', 'mockery', 'assertthat', 'checkmate', 'assertr', 'import', 'lobstr', 'pryr'))
