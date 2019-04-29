ad769236deacf05445527cf393d07389ba2805ca
Tom Bell <tjbell@gmail.com>
2019-04-29 10:51:01 -0600

    test: handle errors

--- tests/reporter-json.R
+++ tests/reporter-json.R
@@ -51,28 +51,34 @@ JsonReporter <- R6::R6Class("JsonReporter",
       errors <- sum(test_results$passed == -1)
       timestamp <- private$timestamp()
 
-      self$cat_line(glue('{{ "testsuites": [{{\n  "suite": "{self$test_context}",\n  "timestamp": "{timestamp}",\n  "suiteTime": "{suiteTime}",\n  "passed": {tolower(as.character(full_pass))},\n  "tests": {tests},\n  "failures": {failures},\n  "errors": {errors},\n'))
-      self$cat_line('  "testResults": [')
+      if(errors <= 0) {
+        self$cat_line(glue('{{ "testsuites": [{{\n  "suite": "{self$test_context}",\n  "timestamp": "{timestamp}",\n  "suiteTime": "{suiteTime}",\n  "passed": {tolower(as.character(full_pass))},\n  "tests": {tests},\n  "failures": {failures},\n  "errors": {errors},\n'))
 
-      for (row in 1:nrow(test_results)) {
-        tag <- test_results[row, "tag"]
-        time  <- test_results[row, "time"]
-        passed <- test_results[row, "passed"]
-        result  <- test_results[row, "result"]
-        comma <- ','
+        self$cat_line('  "testResults": [')
 
-        if (row == nrow(test_results)) {
-          comma <- ''
+        for (row in 1:nrow(test_results)) {
+          tag <- test_results[row, "tag"]
+          time  <- test_results[row, "time"]
+          passed <- test_results[row, "passed"]
+          result  <- test_results[row, "result"]
+          comma <- ','
+
+          if (row == nrow(test_results)) {
+            comma <- ''
+          }
+
+          if(as.logical(passed)){
+            self$cat_line(glue('    {{ "tag": "{tag}", "time": {time}, "passed": {tolower(as.logical(passed))}, "error": {{}} }}{comma}'))
+          } else {
+            self$cat_line(glue('    {{ "tag": "{tag}", "time": {time}, "passed": {tolower(as.logical(passed))}, "error": {{ "message": "{result}"}} }}{comma}'))
+          }
         }
 
-        if(as.logical(passed)){
-          self$cat_line(glue('    {{ "tag": "{tag}", "time": {time}, "passed": {tolower(as.logical(passed))}, "error": {{}} }}{comma}'))
-        } else {
-          self$cat_line(glue('    {{ "tag": "{tag}", "time": {time}, "passed": {tolower(as.logical(passed))}, "error": {{ "message": "{result}"}} }}{comma}'))
-        }
+        self$cat_line('  ]\n}]}')
+      } else {
+        self$cat_line('Execution halted')
       }
-
-      self$cat_line('  ]\n}]}')
+      
     }
   ),
 
